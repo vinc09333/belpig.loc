62fcb68137ec03ee2ae27621a5dee6fad8a0265a
vinc09333 <mr.vinc09333@yandex.ru>
2020-05-09 19:35:19 +0300

    Initialize DI container

--- engine/DI/DI.php
+++ engine/DI/DI.php
@@ -37,6 +37,6 @@ class DI
      */
     public function has($key)
     {
-        return isset($this->container[$key]);
+        return isset($this->container[$key]) ? $this->container[$key] : null;
     }
 }
