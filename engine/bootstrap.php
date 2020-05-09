fd3ba7a332bdb8df24c73f716f2b902f30e89ab3
vinc09333 <mr.vinc09333@yandex.ru>
2020-05-09 19:32:49 +0300

    This .php file starting app

--- engine/bootstrap.php
+++ engine/bootstrap.php
@@ -2,12 +2,21 @@
 
 use Engine\Cms;
 use Engine\DI\DI;
-
+require_once __DIR__ . "/../vendor/autoload.php";
 try{
     /**
      * Dependency Injection
      */
     $di = new DI();
+    $services = require __DIR__ . '/Config/Service.php';
+    /**
+     * Init Service
+     */
+    foreach ($services as $service)
+    {
+        $provider = new $service($di);
+        $provider->init();
+    }
     /**
      * @RUN_CMS
      */
