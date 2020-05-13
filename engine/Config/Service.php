3891dc3ae5d213d7ac967f3dafbe75579b2a5970
vinc09333 <mr.vinc09333@yandex.ru>
2020-05-13 10:55:55 +0300

    Enabled Service List

--- engine/Config/Service.php
+++ engine/Config/Service.php
@@ -2,4 +2,9 @@
 return [
     Engine\Service\Database\Provider::class,
     Engine\Service\Router\Provider::class,
+    Engine\Service\Auth\Provider::class,
+    Engine\Service\View\Provider::class,
+    Engine\Service\QueryBuilder\Provider::class,
+    Engine\Service\Request\Provider::class,
+    //Engine\Service\Config\Provider::class,
 ];
