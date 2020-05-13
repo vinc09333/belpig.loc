50f79f9675a8c2194ac01d8bdce750b8d0de4008
vinc09333 <mr.vinc09333@yandex.ru>
2020-05-13 19:25:59 +0300

    General Controller for cms ENV

--- cms/Controller/CmsController.php
+++ cms/Controller/CmsController.php
@@ -1,23 +1,17 @@
 <?php
 
-
 namespace Cms\Controller;
 
 use Engine\Controller;
-use Engine\DI\DI;
 
 class CmsController extends Controller
 {
     /**
      * CmsController constructor.
-     * @param DI $di
+     * @param \Engine\DI\DI $di
      */
-    public function __construct(DI $di)
+    public function __construct($di)
     {
         parent::__construct($di);
-    }
-    public function header()
-    {
-
     }
 }
