3be4b47c73a92f44f09284aab525d78af609b1ee
vinc09333 <mr.vinc09333@yandex.ru>
2020-05-13 10:46:38 +0300

    Added access to { DB,View,Auth,ORM,Request }class

--- engine/Controller.php
+++ engine/Controller.php
@@ -9,14 +9,27 @@ use Engine\DI\DI;
 abstract class Controller
 {
     /**
-     * @var Engine\DI\DI
+     * @var DI
      */
     protected $di;
-
     protected $db;
-
+    protected $view;
+    protected $auth;
+    protected $request;
+    //protected $config;
+    public $queryBuilder;
+    /**
+     * Controller constructor.
+     * @param DI $di
+     */
     public function __construct(DI $di)
     {
         $this->di = $di;
+        $this->db = $this->di->get('db');
+        $this->view = $this->di->get('view');
+        $this->auth = $this->di->get('auth');
+        $this->queryBuilder = $this->di->get('queryBuilder');
+        $this->request = $this->di->get('request');
+        //$this->config = $this->di->get('config');
     }
 }
