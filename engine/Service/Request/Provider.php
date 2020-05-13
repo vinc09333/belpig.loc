1211256111b0888b5ba0293ff912ee21ffd168e2
vinc09333 <mr.vinc09333@yandex.ru>
2020-05-13 10:52:55 +0300

    This Provider.php add servicename = 'request' in DI container

---
+++ engine/Service/Request/Provider.php
@@ -0,0 +1,23 @@
+<?php
+
+
+namespace Engine\Service\Request;
+
+use Engine\Core\Request\Request;
+use Engine\Service\AbstractProvider;
+
+class Provider extends AbstractProvider
+{
+    /**
+     * @var string
+     */
+    public $serviceName = 'request';
+    /**
+     * @inheritDoc
+     */
+    function init()
+    {
+        $request = new Request();
+        $this->di->set($this->serviceName, $request);
+    }
+}
