c0aa38f1e9d26904f6b5141687f53f2fea8deaaf
vinc09333 <mr.vinc09333@yandex.ru>
2020-05-13 10:51:23 +0300

    This Provider.php add servicename = 'auth' in DI container

---
+++ engine/Service/Auth/Provider.php
@@ -0,0 +1,24 @@
+<?php
+
+
+namespace Engine\Service\Auth;
+
+
+use Engine\Service\AbstractProvider;
+use Engine\Core\Auth\Auth;
+
+class Provider extends AbstractProvider
+{
+    /**
+     * @var string
+     */
+    public $serviceName = 'auth';
+    /**
+     * @inheritDoc
+     */
+    function init()
+    {
+        $auth = new Auth();
+        $this->di->set($this->serviceName, $auth);
+    }
+}
