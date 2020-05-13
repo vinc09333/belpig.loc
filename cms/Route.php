3c3975ee3e384ad00f94a985e7c96d75e4bd57df
vinc09333 <mr.vinc09333@yandex.ru>
2020-05-13 19:23:39 +0300

    PHP File Route.php need to routing ENV

--- cms/Route.php
+++ cms/Route.php
@@ -1,5 +1,6 @@
 <?php
+/**
+ * List routes
+ */
+
 $this->router->add('home', '/', 'HomeController:index');
-$this->router->add('news', '/news', 'HomeController:news');
-$this->router->add('news_single', '/news/(id:int)', 'HomeController:news');
-$this->router->add('admin', '/admin', 'AdminController:index');
